ac54d04466fe9617a6992cf39f427c137cc7f0cb
Daniel <schwartzray8@gmail.com>
2020-07-12 09:34:18 -0500

    working

--- missingmoney2.py
+++ missingmoney2.py
@@ -28,8 +28,7 @@ good_states = ['AL', 'AK', 'AZ', 'AR', 'CO', 'DC',
   'NH', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'RI',
   'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WV', 'WI'] 
 
-files = ['75_Foodlink.csv', '7_PTJ.csv']
-
+files = ['160_Altamarea_Group.csv', '160_American_Transit_Insurance_C.csv', '160_Applied_Data_Finance.csv', '160_Big_Spaceship.csv', '160_Catalyst_Inc.csv', '160_Civic_Entertainment_Group,_LLC_(A_Seacrest_Global_Group_Company.csv', '160_Coalition_for_Hispanic_Family_Service.csv', '160_Concordia_International_Forwarding_Corporatio.csv', '160_DFO_Global_Performance_Commerce.csv', '160_DLC_Management_Corp..csv', '160_Datalo.csv', '160_Dayton_T._Brown,_Inc.csv', '160_DeCrescente_Dist._Co.,_Inc..csv', '160_Deloitte_Tax_LLP.csv', '160_Echidna.csv', '160_Edwin_Gould_Services_for_Children_&_Familie.csv', '160_ExecOnline,_Inc..csv', '160_FIAF_-_French_Institute_Alliance_Francaise.csv', '160_Frenkel_&_Compan.csv', '160_Garfunkel_Wild,_P.C..csv', '160_GroundLin.csv', '160_Hospice_&_Palliative_Care_Buffal.csv', '160_Kindergarten_Kid.csv', '160_MSA_MODEL.csv', '160_Marymount_School_of_New_York.csv', '160_Meadowbrook_Financial_Mortgage_Bankers_Corp..csv', '160_NY_Kids_Club_and_NY_Preschool.csv', '160_NoHo_Hospitality_Group.csv', '160_Norwich_Pharma_Services.csv', '160_ONEGROUP_Risk_Management_and_Insurance.csv', '160_Oswego_Health.csv', '160_Plate.csv', '160_Property_Markets_Group.csv', '160_Public_Preparatory_Networ.csv', '160_Royal_Health_Care.csv', "160_ST._LUKE'S_ROOSEVELT_HOSPITAL.csv", '160_Sailthru.csv', '160_Skyhorse_Publishing,_Inc.csv', '160_Smithsonian_Channel.csv', '160_TDX_Construction_Corporation.csv', '160_The_IMA_Grou.csv', '160_The_Wall_Group.csv', '160_Town_of_Colonie.csv', '160_Vector_Media.csv', '160_Westcon-Comstor_North_America.csv', "160_Women_Presidents'\u200b_Organization.csv", '160_Xaverian_High_School.csv', '160_pymetrics.csv', '170_Blue_Stat.csv', '170_CDG_NY.csv', '170_Democrat_and_Chronicle_-_Part_of_the_USA_Today_Networ.csv', '170_Elite_Dail.csv', '170_Fluent,_In.csv', '170_JUV_Consultin.csv', '170_Kernel,_Created_by_Spectrum_Reac.csv', '170_Optimedia_U.csv', '170_Thrillis.csv', '180_McCann_Health_New_Yor.csv', '180_Mother_New_Yor.csv', '180_Partners_+_Napie.csv', '180_Search_Laboratory_Inc.csv', '180_United_Entertainment_Grou.csv', '18_Amalgamated.csv', '190_121_Graphic_Desig.csv', '190_Influenste.csv', '190_Orion_Worldwid.csv', '190_The_Webby_Award.csv', '190_bayarda.csv', '2,400_Fairfield_Inn_&_Suites.csv', '2,900_Department_of_Public_Wor.csv', '200_9.8_Grou.csv', '200_Captivate,_LL.csv', '200_GALE_Partner.csv', '200_H4B_Chelse.csv', '200_Horizon_Nex.csv', '200_Neon_-_An_FCB_Health_Network_Compan.csv', '200_Remedy_Health_Medi.csv', '32_Rumble.csv', '360_KS&R.csv', '57_21GRAMS.csv', '6_Gravitas.csv', '730_Wartburg.csv', '75_Foodlink.csv', '7_PTJ.csv']
 # for filename in sorted(os.listdir('/Users/danielschwartz/Desktop/simplefuture/SF_Schools')):
 for filename in files:
   try:
@@ -260,6 +259,6 @@ for filename in files:
     filename = filename.split('_')[1:]
     filename = ' '.join(filename)
     filename = filename.replace(" ", "_")
-    df.to_csv(f"/Users/danielschwartz/Desktop/simplefuture/mm_csvs/mm_NY_NoFunding/{mm}_{percent_with_up}_{all_employees}_{filename}")
+    df.to_csv(f"/Users/danielschwartz/Desktop/simplefuture/mm_csvs/mm_NY_NoFunding2/{mm}_{percent_with_up}_{all_employees}_{filename}")
   except:
     continue
